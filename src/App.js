6e797f0dfcaa3619cd7bd2cbf84feedb2ad935bf
Graham Walker <gwalker256@gmail.com>
2021-12-17 15:17:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
-function App() {
+const App = () => {
+  // const [numbers, setNumber] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const buttonValues = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "7",
+    "8",
+    "9",
+    "4",
+    "5",
+    "6",
+    "1",
+    "2",
+    "3",
+    "0",
+    ".",
+    "AC",
+    "=",
+  ];
+
+  const handler = () => {
+    if (userInput === "AC") {
+      setUserInput("");
+    } else if (userInput === "=") {
+      return userInput.evaluate;
+    }
+    // else (add user input to string)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="buttonBox">
+      {buttonValues.map((item, index) => {
+        return (
+          <Card className="calcButtons" index={index} key={index} item={item} />
+        );
+      })}
     </div>
   );
-}
+};
+
+const Card = (props) => {
+  return <h2 className="calcButtons">{props.item}</h2>;
+};
 
 export default App;
